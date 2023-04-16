432f87b9c7a476db59ee33b91d36d6b2111af31e
OlojaMuyiwa <olojam4969@gmail.com>
2023-04-16 23:39:24 +0100

    Add Prisma to project and create User and Bookmark models with relationships in schema.prisma. Create migration for these changes and add Prisma module to app and auth modules with Prisma service provided

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService { }
+export class AuthService {
+  constructor(private prisma: PrismaService) {
+
+  }
+ }
