d20bbbef0c1e0ed6971c6e8b4ee04a4d646c3c6c
OlojaMuyiwa <olojam4969@gmail.com>
2023-04-17 00:01:48 +0100

    Add class-transformer and class-validator dependencies and implement AuthDto for signup route in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common'
+import { Body, Controller, ParseIntPipe, Post, Req } from '@nestjs/common'
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
   @Post('/register')
-  signUp(@Req() req: Request) {
-    console.log(req.body)
+  signUp(@Body() dto: AuthDto) {
+    console.log(dto)
     return this.authService.signup()
   }
 
