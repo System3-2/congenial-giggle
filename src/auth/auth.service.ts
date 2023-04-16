118e766e4059a23c21023daeff38dfc0bd0cc62a
OlojaMuyiwa <olojam4969@gmail.com>
2023-04-17 00:17:31 +0100

    Generate password hash and create user when signing up in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) { }
-  signup(dto: AuthDto) {
-    return { success: true, message: 'Account created succesfully' }
+ async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash
+      }
+    })
+    return user;
   }
 
   login() {
